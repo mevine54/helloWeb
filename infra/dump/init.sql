cc5673cf91ff65188ee4c1c069ff079748f1172a
mevine54 <mevinecascadeur@gmail.com>
2025-02-21 10:08:26 +0100

    hello1

--- infra/dump/init.sql
+++ infra/dump/init.sql
@@ -1,8 +1,3 @@
-CREATE DATABASE springboot;
-use springboot;
-
-DROP TABLE IF EXISTS `Person`;
-
 CREATE TABLE `Person` (
                           `id` int unsigned NOT NULL auto_increment,
                           `lastname` varchar(50) default NULL,
